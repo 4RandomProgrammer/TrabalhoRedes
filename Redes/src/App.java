6adbabb38381447f6ecce4d82b0893784fb37512
LeozeraVal <leovmorales@hotmail.com>
2021-04-10 18:48:53 -0300

    slk mt bom mesmo
    Co-authored-by: Eduardo Ravagnani <link2755@users.noreply.github.com>
    Co-authored-by: ARandomProgrammer <4RandomProgrammer@users.noreply.github.com>

--- Redes/src/App.java
+++ Redes/src/App.java
@@ -1,72 +1,135 @@
-// Leonardo Valerio Morales 771030
+// Caio Peres
+// Eduardo Ravagnani de Melo 771004
+// Leonardo Valerio Morales
+// Luís Felipe Dobner Henriques 771036
 // Exercicio de Redes, comunicacao entre processos por socket
 // Cliente
 
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class App {
+
+    // -------> client aq <------
+
     public static void main(String[] args) throws Exception {
-		// try catch apenas existe para exibir a mensagem de erro caso o usuario cause uma excecao.
-        try {
-            // Instancia um socket de cliente na porta 12345
-            Socket servidor = new Socket("localhost", 12345);
-            System.out.println("Cliente eviando para porta 12345");
 
-            // Cria Scanner para entrada do teclado
-            Scanner teclado = new Scanner(System.in);
-            // Le a proxima linha do teclado no formato char int int
-			System.out.println("Escreva a Requisicao desejada no formato 'operacao numero1 numero2'");
-            char op = teclado.next().charAt(0);
-            int num1 = teclado.nextInt();
-            int num2 = teclado.nextInt();
-            teclado.close();
-    
-            // Cria a Stream de saida para o servidor, escreve um Byte com a operacao
-            // e dois Ints com os operandos
-            DataOutputStream saida = new DataOutputStream(servidor.getOutputStream());
-            saida.writeByte(op);
-            saida.writeInt(num1);
-            saida.writeInt(num2);
-			System.out.println(saida.size());
-            // Flush para enviar.
-            saida.flush();
+        Scanner entrada = new Scanner(System.in);
+        String ip = null;
+        Socket servidor = null;
+        ServerSocket localServer;
 
-            // Recebe uma resposta do servidor
-            DataInputStream resposta_serv = new DataInputStream(servidor.getInputStream());
+        System.out.println("Vc deseja conectar ou hostear a conexão?");
+        System.out.println("1 - para se conectar");
+        System.err.println("2 - para ser host");
 
-            // Separa o caracter de sucesso da resposta em si.
-            char sucesso = (char) resposta_serv.readByte();
-            int resposta = resposta_serv.readInt();
+        int conexao = entrada.nextInt();
 
-            if (sucesso == '=') {
-                // Caso caracter de sucesso esteja valido exibe a resposta e envia uma confirmacao de sucesso ao servidor, e depois fecha a conexao.
-                System.out.println("Resposta do Servidor Recebida, Operacao foi Sucesso!");
-                System.out.println("Resultado da Operacao: " + resposta);
-                System.out.println("Enviando Confirmacao de Sucesso ao Servidor...");
-                saida.write('1');
-                saida.close();
-                servidor.close();
-            } else if (sucesso == '?') {
-                // Caso caracter de sucesso seja invalido apenas envia uma confirmacao de erro ao servidor, e depois fecha a conexao.
-                System.out.println("Resposta do Servidor Recebida, Operacao nao Suportada!");
-                System.out.println("Enviando Confirmacao de Falha ao Servidor...");
-                saida.write('n');
-                saida.close();
-                servidor.close();
-                return;
-            } else {
-                // Caso nao receba resposta, envia uma confirmacao de erro ao servidor, e depois fecha a conexao.
-                System.out.println("Resposta do Servidor Nao Recebida!");
-                System.out.println("Enviando Confirmacao de Falha ao Servidor...");
-                saida.write('n');
-                saida.close();
-                servidor.close();
-            }
+        if(conexao == 1){
+            //cliente
+            ip = entrada.nextLine();
+            servidor = new Socket(ip, 7777);
+            System.out.println("Cliente eviando para porta 77777");
+        } else {
+            //host
+            localServer = new ServerSocket(7777);
+            servidor = localServer.accept();
+            System.out.println("Conectado com: " + servidor.getInetAddress().getHostAddress());//notifica caso haja conexao
 
-        } catch (Exception e) {
-            System.out.println("Erro:" + e.getMessage());
         }
+
+
+        Runnable r2 = new EnviarData(servidor);
+        new Thread(r2).start();
+
+        Runnable r3 = new ReceberData(servidor);
+        new Thread(r3).start();
+
+        entrada.close();
+
+    }
+    
+    public static class EnviarData implements Runnable {
+
+
+        Socket servidor;
+
+        public EnviarData(Socket servidor) {
+            this.servidor = servidor;
+        }
+        
+        @Override
+        public void run(){
+            OutputStream saida = servidor.getOutputStream();
+            while (true) {
+                try {
+                    // Le a proxima linha do teclado no formato char int int
+                    System.out.println("Escreva uma mensagem :D");
+                    //ESPERA DA UI O INPUT EM STRING string
+                    String string = new String("Carlos");
+        
+                    // Cria a Stream de saida para o servidor, escreve um Byte com a operacao
+                    // e dois Ints com os operandos
+                    saida.write(b);
+                    // Flush para enviar.
+                    saida.flush();   
+                    
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    };
+    
+    public static class ReceberData implements Runnable {
+
+        Socket servidor;
+
+        public ReceberData(Socket servidor) {
+            this.servidor = servidor;
+        }
+        
+
+        @Override
+        public void run(){
+
+            InputStream in = null;
+            
+            try {
+                in = servidor.getInputStream();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            while (true) {
+                try {
+                    //estabelece a stream in para receber dados
+    
+                    while(in.available() == 0){
+                        //so estamos esperando XD
+                        Thread.sleep(10);
+                    }
+    
+                    //le a mensagem envida
+                    byte[] msg = new byte[500];
+                    in.read(msg);
+    
+                    //convertendo mensagem para string
+                    String s = new String(msg, StandardCharsets.UTF_8);
+    
+                    //enviar para interface
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            //in.close();
+        }
+    };
+
+    public class Bundinha {
+
     }
 }
+
